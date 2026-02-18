c0cc68b171309c7223adf749fe6e2e663b15081e
Deadstar <ilasaigues@gmail.com>
2026-02-18 17:35:22 -0300

    Added base states with most behaviours missing

---
+++ Assets/Scripts/BehaviourMachine/Implementations/PlayerFallingBehaviour.cs
@@ -0,0 +1,27 @@
+public class PlayerFallingBehaviour : PlayerAirBehaviour
+
+{
+    public PlayerFallingBehaviour(PlayerController playerController) : base(playerController)
+    {
+    }
+
+    public override void Enter()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void Exit()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void FixedUpdate(float delta)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override void Update(float delta)
+    {
+        throw new System.NotImplementedException();
+    }
+}
---
+++ Assets/Scripts/BehaviourMachine/Implementations/PlayerGroundedBehaviour.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+public abstract class PlayerGroundedBehaviour : BasePlayerBehaviour
+{
+    protected PlayerGroundedBehaviour(PlayerController player) : base(player)
+    {
+    }
+}
