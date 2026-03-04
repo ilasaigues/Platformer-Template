52de1325c1595e2e42e846ca020cb623155c6e97
Deadstar <ilasaigues@gmail.com>
2026-03-04 11:50:15 -0300

    Fixed overlapping error in collideAndSlide

--- Assets/Scripts/CollisionController.cs
+++ Assets/Scripts/CollisionController.cs
@@ -40,13 +40,24 @@ public class CollisionController : MonoBehaviour
         RaycastHit2D[] hits = new RaycastHit2D[5];
         int hitCount = Physics2D.BoxCast(position, bounds.size, 0, vel.normalized, filter, hits, dist);
 
+        Vector2 overlapCorrection = Vector2.zero;
         for (int i = 0; i < hits.Length; i++)
         {
             var hit = hits[i];
             if (hit)
             {
-                if (hit.distance * hit.fraction == 0)
+                if (hit.distance * hit.fraction == 0) // hit is result of overlapping origin of boxcast and collider
                 {
+                    var hitVector = hit.point - position;
+
+                    if (Mathf.Abs(vel.y) > Mathf.Abs(vel.x)) // moving vertically
+                    {
+                        overlapCorrection += 2 * -hitVector.y.Sign0() * (Mathf.Abs(hitVector.y) - bounds.size.y / 2) * Vector2.up;
+                    }
+                    else // moving horizontally
+                    {
+                        overlapCorrection += 2 * -hitVector.x.Sign0() * (Mathf.Abs(hitVector.x) - bounds.size.x / 2) * Vector2.right;
+                    }
                     Debug.DrawLine(position, hit.point, Color.red, .1f);
                 }
             }
@@ -62,7 +73,7 @@ public class CollisionController : MonoBehaviour
             float mag = leftover.magnitude;
             leftover = Vector3.ProjectOnPlane(leftover, closestHit.normal);//.normalized;
             //leftover *= mag;
-            return snapToSurface + CollideAndSlideVel(position + snapToSurface, bounds, leftover, filter, recursionDepth + 1);
+            return snapToSurface + CollideAndSlideVel(position + snapToSurface, bounds, leftover, filter, recursionDepth + 1) + overlapCorrection;
         }
 
         return vel;
